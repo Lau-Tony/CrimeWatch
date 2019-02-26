10b52916b538e91fa66ed4a885c4896b143b9b56
Tony <tonyywlau@gmail.com>
2019-02-26 11:23:20 -0800

    initial commit

---
+++ app/src/main/java/com/example/crimewatch/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.crimewatch;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void crimeMapButton(View v)
+    {
+        Button crimeButton = (Button) findViewById(R.id.crime_map_button);
+        crimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, CrimeMap.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void settingsButton(View v)
+    {
+        Button settingsButton = (Button) findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Settings.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void aboutUsButton(View v)
+    {
+        Button aboutUsButton = (Button) findViewById(R.id.about_us_button);
+        aboutUsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AboutUs.class);
+                startActivity(i);
+            }
+        });
+    }
+}
